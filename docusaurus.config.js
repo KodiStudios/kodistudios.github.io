3daefb991f31943cacf85207b17362556a6f1531
KodiStudios <60270547+KodiStudios@users.noreply.github.com>
2023-03-02 19:28:47 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KodiStudios', // Usually your GitHub org/user name.
   projectName: 'kodistudios.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages'
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false, // ???
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
