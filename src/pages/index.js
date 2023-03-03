5879a65d999a7b52c13f9505253dc9ccad62ec5a
Nikolai Aristov <KodiStudios@gmail.com>
2023-03-02 20:16:32 -0800

    Description and Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,8 +23,9 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Kodi Studios - Less Code, More Delight">
+        {/* Description will go into a meta tag in <head /> */}
       <HomepageHeader />
       <main>
         <HomepageFeatures />
