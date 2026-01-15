700074dfe5ecd4baa6321f6ea5842229bc1c8fb6
Nikolai Aristov <kodistudios@mgmail.com>
2026-01-14 21:47:10 -0800

    fix: update site URL to kodistudios.com and add CNAME file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/KodiStudiosLogo.ico',
 
   // Set the production url of your site here
-  url: 'https://kodistudios.github.io',
+  url: 'https://kodistudios.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
