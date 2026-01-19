02b6874218ee0e7959ca3337668fb5cc1493310f
Nikolai Aristov <kodistudios@mgmail.com>
2026-01-19 14:54:35 -0800

    feat: update Docusaurus configuration to use markdown hooks for handling broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,8 +20,13 @@ const config: Config = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
+
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
